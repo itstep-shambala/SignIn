f03adb8955a11006e473acda6f2dd4aab471f0b2
ssokolovaaaa <ssokolovaaaa@gmail.com>
2021-02-22 22:17:56 +0700

    Добавили журналирование в сравнение паролей

--- Registration/RegUser.cs
+++ Registration/RegUser.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using Logging;
 namespace Registration
 {
     public class RegUser
     {
+        Log log = new Log();
         public bool PasswordVerification (string password, string passwordCheck)
         {
             if (password == passwordCheck)
             {
+                log.Success("Пароли совпадают");
                 return true;
             }
+            log.Error("Пароли не совпадают");
             return false;
         }
     }
