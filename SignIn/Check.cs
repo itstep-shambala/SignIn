8797e7290d3691e5c71fb73986923f97be3698e6
ssokolovaaaa <ssokolovaaaa@gmail.com>
2021-02-24 20:57:36 +0700

    Проверка пароля

---
+++ SignIn/Check.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignIn
+{
+    class Check
+    {
+        public bool Password(string password, int min)
+        {
+            int x;
+            x = password.Length;
+            if (x >= min)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool Alphabet(string password)
+        {
+            bool flag = true;
+            int alp = 0;
+            int num = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] >= 'A' && password[i] <= 'Z')
+                {
+                    alp += 1;
+                }
+                if (password[i] >= '0' && password[i] <= '9')
+                {
+                    num += 1;
+                }
+                if (!(password[i] >= 'a' && password[i] <= 'z' || password[i] >= 'A' && password[i] <= 'Z' || password[i] >= '0' && password[i] <= '9'))
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag == true && num >= 1 && alp >= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
